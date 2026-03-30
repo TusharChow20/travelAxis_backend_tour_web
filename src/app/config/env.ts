6d58dbbf04b44545b65438be33aae472f65d83ae
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-03-31 00:39:53 +0600

    jwt token validation

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -4,5 +4,8 @@ const varEnv = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES,
+  BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND,
 };
 export default varEnv;
