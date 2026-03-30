efd47bbc9b97c981c4dd06cdd47221e6a30c4723
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-03-30 23:45:58 +0600

    route set with middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,7 +3,8 @@ import { UserControllers } from "./user.controller";
 import { ZodObject } from "zod";
 import { createUserSchemaZodValidation } from "./user.validation";
 import { validateUserRequest } from "../../middlewares/userValidateRequest";
-
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Role } from "./user.interface";
 const router = Router();
 
 router.post(
@@ -12,6 +13,28 @@ router.post(
   UserControllers.createUser,
 );
 
-router.get("/all-users", UserControllers.getAllUser);
+router.get(
+  "/all-users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw Error("Unauthorized");
+      }
+      const verifyToken = jwt.verify(token, "secret");
+
+      if (
+        (verifyToken as JwtPayload).role !== (Role.ADMIN || Role.SUPER_ADMIN)
+      ) {
+        throw new Error("Forbidden: Insufficient permissions");
+      }
+      console.log(verifyToken);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  UserControllers.getAllUser,
+);
 
 export const UserRoutes = router;
