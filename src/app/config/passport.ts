5c66523423079351da067c07b6db2e1170d2cbe8
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-04-01 23:23:11 +0600

    google auth added

---
+++ src/app/config/passport.ts
@@ -0,0 +1,53 @@
+import passport from "passport";
+import {
+  Strategy as GoogleStrategy,
+  Profile,
+  VerifyCallback,
+} from "passport-google-oauth20";
+import varEnv from "./env";
+import { User } from "../modules/user/user.model";
+import { Role } from "../modules/user/user.interface";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: varEnv.GOOGLE_CLIENT_ID as string,
+      clientSecret: varEnv.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: varEnv.GOOGLE_CALLBACK as string,
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback,
+    ) => {
+      try {
+        const email = profile.emails?.[0]?.value;
+        if (!email) {
+          return done(null, false, { message: "No email found" });
+        }
+        let user = await User.findOne({ email });
+        if (!user) {
+          user = await User.create({
+            email,
+
+            name: profile.displayName,
+            picture: profile.photos?.[0]?.value || "",
+            role: Role.USER,
+            isVerified: true,
+            auths: [
+              {
+                provider_id: profile.id,
+                provider_name: "google",
+              },
+            ],
+          });
+        }
+        return done(null, user);
+      } catch (error) {
+        console.log("Error in google credentials");
+        return done(error);
+      }
+    },
+  ),
+);
