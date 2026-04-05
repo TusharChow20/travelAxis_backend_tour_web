9c82d9bfe4f89b319ef6b02a9ddb29482ef684fd
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-04-05 13:05:03 +0600

    Tour create

---
+++ src/app/modules/tour/tour.controller.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { TourService } from "./tour.service";
+import { sendResponse } from "../../utils/sendResponse";
+
+const createTour = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const tour = await TourService.createTour(req.body);
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "User Inserted Successfully",
+      data: tour,
+    });
+  },
+);
+export const TourController = {
+  createTour,
+};
