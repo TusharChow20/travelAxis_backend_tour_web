8af735d5a6bdbe43243a2b815f4e39f8687be1a5
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-04-10 23:56:56 +0600

    update payment

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,11 +1,21 @@
+import { PAYMENT_STATUS } from "../payment/payment.interface";
+import { Payment } from "../payment/payment.model";
 import { Tour } from "../tour/tour.model";
 import { User } from "../user/user.model";
 import { BOOKING_STATUS, IBooking } from "./booking.interface";
 import { Booking } from "./booking.model";
-
+const getTransactionId = () => {
+  return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`;
+};
 const createBooking = async (payload: Partial<IBooking>, userId: string) => {
   try {
+    const transactionId = getTransactionId();
     const user = await User.findById(userId);
+
+    if (!user?.phone || !user.address) {
+      throw new Error("Need Phone Number &&&& Address");
+    }
+
     const tour = await Tour.findById(payload.tour).select("costFrom");
     const amount = Number(tour?.costFrom) * Number(payload.peopleCount);
     const booking = await Booking.create({
@@ -14,6 +24,22 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
       status: BOOKING_STATUS.PENDING,
       ...payload,
     });
+
+    const payment = await Payment.create({
+      booking: booking._id,
+      status: PAYMENT_STATUS.UNPAID,
+      transactionId: transactionId,
+      amount: amount,
+    });
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      booking._id,
+      { payment: payment._id },
+      { new: true, runValidators: true },
+    )
+      .populate("user", "name email phone address")
+      .populate("tour", "title costFrom")
+      .populate("payment");
+    return updatedBooking;
   } catch (error) {
     console.log(error);
   }
