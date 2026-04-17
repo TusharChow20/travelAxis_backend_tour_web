17e2d8ef214ecdbabbbc5efb3bd89f03798c09fb
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-04-17 17:30:48 +0600

    delete the file from cloud if any errors aoccued

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
+import { deleteImageFromCloud } from "../config/cloudinary.config";
 
-export const globalErrorHandler = (
+export const globalErrorHandler = async (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
+  if (req.file?.path) {
+    await deleteImageFromCloud(req.file.path);
+  }
+
+  // Delete multiple files on error
+  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    const imageUrls = (req.files as Express.Multer.File[]).map(
+      (file) => file.path, //
+    );
+    await Promise.all(imageUrls.map((url) => deleteImageFromCloud(url)));
+  }
 
   if (err instanceof ZodError) {
     const formattedErrors = err.issues.map((e) => ({
